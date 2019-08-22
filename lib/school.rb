7a6fceaf4c2d303549d2a61fbf82431676a61812
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-22 21:44:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class School
   attr_accessor :name, :roster 
   attr_reader :grade
@@ -18,3 +19,19 @@ class School
     end 
   end 
 end
+=======
+class School 
+  attr_accessor :name , :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+def add_student(student, level)
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
+    roster[level] ||= []
+    roster[level] << student
+  end 
+end 
+>>>>>>> 9722f8551dc7fc3ebf31c5193054d8fccbf69088
